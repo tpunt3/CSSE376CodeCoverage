af38891e9e8757b362927a9a26fb4650b987da0c
Trent Punt <tpunt3@gmail.com>
2015-04-15 23:10:53 -0400

    Task4

--- src/ExpediaTest/UserTest.java
+++ src/ExpediaTest/UserTest.java
@@ -115,6 +115,22 @@ public class UserTest
 		this.target.book(new Booking[]{new Flight(StartDate, EndDate, 100),new Hotel(5), new Car(3)});
 		assertEquals(1024.65,this.target.Price(),0.01);
 	}
+	
+	@Test
+	public void TestThatDiscountInitializes2(){
+		Discount target = new Discount(0.01, 10000);
+		ServiceLocator.Instance().AddDiscount(target);
+		this.target.book(new Booking[]{new Flight(StartDate, EndDate, 100),new Hotel(5), new Car(3)});
+		assertEquals(1035.0,this.target.Price(),0.01);
+	}
+	
+	@Test
+	public void TestThatDiscountInitializes3(){
+		Discount target = new Discount(-0.01, 1);
+		ServiceLocator.Instance().AddDiscount(target);
+		this.target.book(new Booking[]{new Flight(StartDate, EndDate, 100),new Hotel(5), new Car(3)});
+		assertEquals(1035.0,this.target.Price(),0.01);
+	}
 
 	
 	@After
